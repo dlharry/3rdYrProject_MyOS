41da32155c5c2362f0df02fd69b3bd5b13c94495
Hanmin <dlharry@126.com>
2024-02-27 14:32:58 +0000

    support printf

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,26 @@
 #include "common.h"
 #include "mini_uart.h"
+#include "printf.h"
+
+// To support Kustaa Nyholm's printf
+void putc(void *p, char c){
+    if(c == '\n'){
+        uart_send('\r');
+    }
+
+    uart_send(c);
+}
 
 void kernel_main(){
     uart_init();
-    uart_send_string("Hello My OS!\n");
+    init_printf(0, putc);
+    printf("Hello My OS!\n");
 
 #if RPI_VERSION == 4
-    uart_send_string("\tRunning on Rpi 4\n");
+    printf("\tRunning on Rpi 4\n");
 #endif
 
-    uart_send_string("\nInitialization Done\n");
+    printf("\nInitialization Done\n");
 
     while(1){
         uart_send(uart_recv());
