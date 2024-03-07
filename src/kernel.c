49cbf82bc8fcb1dfc0c026f775f6d829790d3b04
Hanmin <dlharry@126.com>
2024-03-07 01:05:06 +0000

    mailbox but no bool

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include "printf.h"
 #include "irq.h"
 #include "timer.h"
+#include "mailbox.h"
 
 // To support Kustaa Nyholm's printf
 void putc(void *p, char c){
@@ -38,6 +39,31 @@ void kernel_main(){
     printf("Sleeping for 2000 ms...\n");
     timer_sleep(2000);
     printf("Sleeping test done.\n");
+
+    printf("MAILBOX:\n");
+    printf("CORE CLOCK: %d\n", mailbox_clock_rate(CT_CORE));
+    printf("EMMC CLOCK: %d\n", mailbox_clock_rate(CT_EMMC));
+    printf("UART CLOCK: %d\n", mailbox_clock_rate(CT_UART));
+    printf("ARM CLOCK: %d\n", mailbox_clock_rate(CT_ARM));
+
+    printf("I2C POWER STATE:\n");
+
+    for(int i=0; i<3; ++i){
+        bool on = mailbox_power_check(i);
+
+        printf("POWER DOMAIN STATUS FOR %d = %d\n", i, on);
+    }
+
+    u32 max_temp = 0;
+
+    mailbox_generic_command(RPI_FIRMWARE_GET_MAX_TEMPERATURE, 0, &max_temp);
+    
+    while(1) {
+        u32 cur_temp = 0;
+        mailbox_generic_command(RPI_FIRMWARE_GET_TEMPERATURE, 0, &cur_temp);
+        printf("Cur temp: %dC MAX: %dC\n", cur_temp/1000, max_temp/1000);
+        timer_sleep(1000);
+    }
 
     printf("\nType something and you shall see response.\n");
     // test the uart handler
